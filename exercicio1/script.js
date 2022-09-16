6146f3e4f99a03cc106ad4c622ccbe5e008710f9
Clara <claradsfcarneiro@gmail.com>
2022-09-16 14:29:18 -0300

    Realizar exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,41 @@
+//a. Declare uma função que imprima Olá, [SEU NOME]!. Chame esta função.
+/*
+function imprimirNome(nome) {
+    console.log(`Olá, ${nome}`);
+}
+
+imprimirNome('Clara');
+*/
+
+//b. Declare uma função que imprima a tabuada do 6. Chame esta função.
+/*
+function tabuadaDoSeis() {
+    for (i = 1; i <= 10; i++) {
+        console.log(`6 x ${i} = ${6*i}`);
+    }
+}
+
+tabuadaDoSeis();
+*/
+
+//c. Comente as funções criadas acima, e reescreva-as utilizando expressões de função, ou arrow functions.
+
+//Função A utilizando expressão de função
+/*
+const imprimirNome = function(nome){
+    console.log(`Olá, ${nome}`);
+}
+
+imprimirNome('Clara');
+*/
+
+//Função B utilizando arrow function 
+/*
+const tabuadaDoSeis = () => {
+    for (i = 1; i <= 10; i++) {
+        console.log(`6 x ${i} = ${6*i}`);
+    }
+}
+
+tabuadaDoSeis();
+*/
